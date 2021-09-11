ccbe4619fb910f7ad8ce83fba5cab94e1088b77b
jiikko <n905i.1214@gmail.com>
2021-09-11 12:39:12 +0900

    a

--- lib/pbmenv.rb
+++ lib/pbmenv.rb
@@ -2,19 +2,26 @@
 
 require_relative "pbmenv/version"
 require_relative "pbmenv/cli"
+require_relative "pbmenv/pbm"
 
 module Pbmenv
   class Error < StandardError; end
-  # Your code goes here...
+  PBM_DIR = "/usr/share/pbm"
 
   def self.init
-    "mkdir -p /usr/share/pbm"
+    "mkdir -p #{PBM_DIR}"
   end
 
   def self.available_versions
+    Pbmenv::PBM.new.available_versions.map { |x| x["name"] =~ /^v([\d.]+)/ && $1 }.compact.each do |v|
+      puts v
+    end
   end
 
   def self.versions
+    Pbmenv::PBM.new.versions.map { |name| name =~ /^v([\d.]+)/ && $1 }.compact.each do |v|
+      puts v
+    end
   end
 
   def self.install(version)
---
+++ lib/pbmenv/pbm.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'json'
+
+module Pbmenv
+  class PBM
+    def available_versions
+      response = URI.open 'https://api.github.com/repos/splaplapla/procon_bypass_man/tags'
+      JSON.parse(response.read)
+    end
+
+    def versions
+      Dir.glob("#{Pbmenv::PBM_DIR}/v*")
+    end
+
+    def install
+    end
+  end
+end
